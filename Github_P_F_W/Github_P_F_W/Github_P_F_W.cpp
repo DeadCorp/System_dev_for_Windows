866a700567a3a6d2d4af12b592a4c3ae3b3cf541
cipid <cipid@DESKTOP-NQMD5UR>
2019-09-23 16:39:59 +0300

    as

--- Github_P_F_W/Github_P_F_W/Github_P_F_W.cpp
+++ Github_P_F_W/Github_P_F_W/Github_P_F_W.cpp
@@ -25,8 +25,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
-	іаіваіваіаі
-
+	
+	asdas
 	// TODO: Разместите код здесь.
 
 	// Инициализация глобальных строк
