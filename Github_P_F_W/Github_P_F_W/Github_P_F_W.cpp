1fd138475b4440659d61ec0ec3496eaeb37e98cd
cipidris <cipidris@gmail.com>
2019-12-02 16:09:33 +0200

    a

--- Github_P_F_W/Github_P_F_W/Github_P_F_W.cpp
+++ Github_P_F_W/Github_P_F_W/Github_P_F_W.cpp
@@ -19,19 +19,14 @@ using namespace std;
 
 // Глобальные переменные:
 HINSTANCE hInst;                                // текущий экземпляр
-const TCHAR szTitle[MAX_LOADSTRING] = _T("Lab_5");                  // Текст строки заголовка
-const TCHAR szWindowClass[MAX_LOADSTRING] = _T("Migal Mykola");            // имя класса главного окна
-const TCHAR text[MAX_LOADSTRING] = _T("TUT SHOTO NAPISANO");
+const TCHAR szTitle[MAX_LOADSTRING] = _T("Lab_6");                  // Текст строки заголовка
+const TCHAR szWindowClass[MAX_LOADSTRING] = _T("Mudri slowa");            // имя класса главного окна
+const TCHAR text[MAX_LOADSTRING] = _T("text text text");
 HCURSOR CUR1,CUR2,CUR0;
-PROCESS_INFORMATION processInfo;
-//OPENFILENAME ofn;
-//TCHAR szFile[260];
-int i = 0;
+int chislo;
 
-int sec = 0;
-wchar_t secx[MAX_LOADSTRING];
-wchar_t secy[MAX_LOADSTRING];
 
+wchar_t chislo_s[MAX_LOADSTRING];
 // Отправить объявления функций, включенных в этот модуль кода:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
@@ -124,10 +119,10 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	HWND hWnd = CreateWindowW(szWindowClass,
 		szTitle,
 		WS_OVERLAPPEDWINDOW,
-		700,
-		400,
-		365,
-		300,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
 		NULL,
 		NULL,
 		hInstance,
@@ -175,108 +170,22 @@ LRESULT CALLBACK  WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	static TEXTMETRIC tm; // структура для записи метрик
 						  // шрифта
 	static POINT pt;//для захвата курсора
+
+
+	
+
 	switch (message)
 	{
 	case WM_KEYDOWN:
 	{
-		// Получаем текущие экранные
-		// координаты курсора мыши 
-		GetCursorPos(&pt);
-
-		// Преобразуем экранные координаты
-		// в оконные координаты
-		ScreenToClient(hWnd, &pt);
-
-		// Для клавиш позиционирования текстового
-		// курсора изменяем соответствующим образом
-		// координаты курсора мыши
-		switch (wParam)
-		{
-		case VK_DOWN:  // вниз
-		{
-			pt.y += 20;
-			break;
-		}
-		case VK_UP:    // вверх
-		{
-			pt.y -= 20;
-			break;
-		}
-		case VK_LEFT:  // влево
-		{
-			pt.x -= 20;
-			break;
-		}
-		case VK_RIGHT: // вправо
-		{
-			pt.x += 20;
-			break;
-		}
-		case VK_RETURN :
-		{
-			
-			SendMessage(hWnd, WM_LBUTTONDOWN, MK_LBUTTON, lParam);
-			//SendMessage(hWnd, WM_LBUTTONDOWN, wParam, lParam);
-			 
-			break;
-		}
-		case VK_SPACE:
-		{
-			i = 1;
-			break;
-		}
-		case VK_SHIFT:
-		{
-			i = 2;
-			break;
-		}
-		case VK_ESCAPE: {
-			i = 0;
-			break;
-		}
-		// Для всех остальных клавиш
-		// ничего не делаем
-		default:
-		{
-			return 0;
-		}
-		}
-
-		// Получаем координаты внутренней
-		// области окна
-		GetClientRect(hWnd, &rc);
-
-		// Вычисляем новые координаты курсора мыши
-		// таким образом, чтобы курсор не выходил
-		// за пределы окна
-		pt.x = max(min(pt.x, rc.right), rc.left);
-		pt.y = max(min(pt.y, rc.bottom), rc.top);
-
-		// Преобразуем оконные координаты в экранные
-		ClientToScreen(hWnd, &pt);
-
-		// Устанавливаем курсор мыши
-		// в новую позицию
-		SetCursorPos(pt.x, pt.y);
-
+		
 		return 0;
 	}
 	case WM_CREATE :
 	{
-		hBtn = CreateWindow(_T("button"), _T( "Вибрати файл для запуску"),
-			WS_CHILD | WS_VISIBLE | WS_BORDER,
-			10, 30, 325, 30, hWnd, 0, hInst, NULL);
-		ShowWindow(hBtn, SW_SHOWNORMAL);
-		hBtn2 = CreateWindow(_T("button"), _T("Iнформація про запущені програмою процеси"),
-			WS_CHILD | WS_VISIBLE | WS_BORDER,
-			10, 90, 325, 30, hWnd, 0, hInst, NULL);
-		ShowWindow(hBtn, SW_SHOWNORMAL);
 		hdc = GetDC(hWnd);
 		
 		
-			CUR1 = LoadCursor(hInst, MAKEINTRESOURCE(IDC_CURSOR1)),
-			CUR2 = LoadCursor(hInst, MAKEINTRESOURCE(IDC_CURSOR2)),
-			CUR0 = LoadCursor(hInst, IDC_HELP);
 		
 		hBmp = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_BITMAP1));
 		GetObject(hBmp, sizeof(BITMAP), &bmp);
@@ -291,104 +200,18 @@ LRESULT CALLBACK  WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	
 	case WM_LBUTTONDOWN :
 	{
-		/*if (wParam == MK_LBUTTON)
-		{
-			/*COLORREF bkcolor = RGB(rand() % 256, rand() % 256, rand() % 256);
-			if (bkbrush)
-				DeleteObject(bkbrush);
-			bkbrush = CreateSolidBrush(bkcolor);
-			SetClassLongPtr(hWnd, GCL_HBRBACKGROUND, (LONG)bkbrush);
-			InvalidateRect(hWnd, NULL, TRUE);
-	
-			PAINTSTRUCT ps;
-			RECT rc;
-			HDC hdc = BeginPaint(hWnd, &ps);
-			GetClientRect(hWnd, &rc);
-			SetDCBrushColor(hdc, bkcolor);
-			FillRect(hdc, &rc, (HBRUSH)GetStockObject(DC_BRUSH));
-
-			//or use ps.rcPaint to repaint only the section which requires update
-			//FillRect(hdc, &ps.rcPaint, (HBRUSH)GetStockObject(DC_BRUSH));
-
-			EndPaint(hWnd, &ps);
-	*/
-			
-
-		//}
-	}
-	case WM_SETCURSOR:
-	{
-		if (i == 1) {
-			SetCursor(CUR1);
-			break;
-		}
-		if (i == 2) {
-			SetCursor(CUR2);
-			break;
-		}
-		if (i == 0) {
-			SetCursor(CUR0);
-			
-		}
+		
+		
+		chislo += 1;
+		InvalidateRect(hWnd, NULL, TRUE);
+		
 		
 
-
-
 	}
+	
 	
 	case WM_COMMAND:
 	{
-		
-		if (lParam == (LPARAM)hBtn)    // если нажали на кнопку
-		{
-			OPENFILENAME ofn = { sizeof ofn };
-			wchar_t file[1024];
-			file[0] = '\0';
-			ofn.lpstrFile = file;
-			ofn.lpstrFileTitle = ofn.lpstrFile;
-			ofn.nMaxFile = 1024;
-			ofn.Flags = OFN_ALLOWMULTISELECT | OFN_EXPLORER;
-			if (GetOpenFileName(&ofn) == TRUE)
-			{
-				
-				
-				
-				STARTUPINFO si;
-				PROCESS_INFORMATION pi;
-				ZeroMemory(&si, sizeof(si));
-				si.cb = sizeof(si);
-				ZeroMemory(&pi, sizeof(pi));
-				
-				if (CreateProcess(NULL, ofn.lpstrFile,
-					NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi) == TRUE)
-				{
-						lstrcat(secx, L"//////////////////////////////////////////////////////////////////////////");
-						lstrcat(secx, L"\nІм'я файлу : ");
-						lstrcat(secx, ofn.lpstrFileTitle);
-						lstrcat(secx, L"\nАйді процесу : ");
-						sec = pi.dwProcessId;
-						_itow_s(sec, secy, 10);	
-						lstrcat(secx, secy);
-						sec = pi.dwThreadId;
-						_itow_s(sec, secy, 10);
-						lstrcat(secx, L"\nАйді потоку : ");
-						lstrcat(secx, secy);
-						lstrcat(secx, L"\n");
-						CloseHandle(pi.hProcess);
-						CloseHandle(pi.hThread);
-				}
-				
-
-			}
-		}
-		if (lParam == (LPARAM)hBtn2)
-		{
-			
-
-			MessageBox(hWnd, secx , L"Інформація про процес                                        ", MB_OK);
-
-		}
-		
 		
 		int wmId = LOWORD(wParam);
 		// Разобрать выбор в меню:
@@ -397,15 +220,7 @@ LRESULT CALLBACK  WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		
 		case IDM_ABOUT:
 			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
-		case IDM_CUR:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, About);			
-			break;
-		case IDM_AUTOR:
-		{
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG2), hWnd, About);
-			break;
-		}
+			break;			
 		case IDM_EXIT:
 			DestroyWindow(hWnd);
 			break;
@@ -416,72 +231,25 @@ LRESULT CALLBACK  WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	break;
 	case WM_PAINT:
 	{ 
-		//int i = tm.tmHeight;          //напевно середнє значення величини символу рядка
-		//int j = tm.tmInternalLeading; //напевно величина простору над символом для спеціальних знаків
-		//int x = SM_CXDOUBLECLK;       //Ширина прямокутника дабл клик
-		//int y = SM_CYDOUBLECLK;       //Висота прямокутника дабл клик
-		//int v = ASPECTX;              //відносна ширина точки
-
-		//wchar_t xstr[100]; //Ширина прямокутника дабл клик
-		//wchar_t ystr[100]; //Висота прямокутника дабл клик
-		//wchar_t istr[100]; //напевно середнє значення величини символу рядка
-		//wchar_t jstr[100]; //напевно величина простору над символом для спеціальних знаків
-		//wchar_t vstr[100]; //відносна ширина точки
-
-		//
-		//
-		//_itow_s(x, xstr, 10);
-		//_itow_s(y, ystr, 10);
-		//_itow_s(i, istr, 10);
-		//_itow_s(j, jstr, 10);
-		//_itow_s(v, vstr, 10);
-		 
-		
-		HBITMAP bmLogo1, bmLogo2;
-		
-
 		
 		PAINTSTRUCT ps;
 		RECT rt;
 		HGDIOBJ hOld;
 		HDC hMemDC,hdc = BeginPaint(hWnd, &ps);
-		
-		
-		
-		// TODO: Добавьте сюда любой код прорисовки, использующий HDC...
-		GetClientRect(hWnd, &rt);		
-		
-		hMemDC = CreateCompatibleDC(hdc);
-		hOld = SelectObject(hMemDC, hBmp);
+		// Получаем текущие экранные
+		// координаты курсора мыши 
+		GetCursorPos(&pt);
 
-		/*StretchBlt(
-			hdc,
-			0, 0,
-			cxClient, cyClient,
-			hMemDC,
-			0, 0,
-			bmp.bmWidth, bmp.bmHeight,
-			SRCCOPY);*/
+		// Преобразуем экранные координаты
+		// в оконные координаты
+		ScreenToClient(hWnd, &pt);
+		//ClientToScreen(hWnd, &pt);
 
-		SelectObject(hMemDC, hOld);
-		DeleteDC(hMemDC);
+			
 		
+		_itow_s(chislo, chislo_s, 10);
+	TextOut(hdc, pt.x, pt.y, chislo_s , wcslen(chislo_s));
 		
-		
-		
-
-		/*TextOut(hdc, 19, 0, L"Ширина прямокутника дабл клик", 30);
-		TextOut(hdc, 19, 18, L"Висота прямокутника дабл клик" , 30);
-		TextOut(hdc, 19, 36, L"напевно середнє значення величини символу рядка" , 48);
-		TextOut(hdc, 19, 54, L"напевно величина простору над символом для спеціальних знаків" , 62);
-		TextOut(hdc, 19, 72, L"відносна ширина точки", 22);*/
-		
-
-		/*TextOut(hdc, 0, 0 , xstr, sizeof(*xstr));
-		TextOut(hdc, 0, 18, ystr, sizeof(*ystr));
-		TextOut(hdc, 0, 36, istr, sizeof(*istr));
-		TextOut(hdc, 0, 54, jstr, sizeof(*jstr));
-		TextOut(hdc, 0, 72, vstr, sizeof(*vstr));*/
 
 
 
@@ -522,26 +290,7 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	
 	case WM_COMMAND: {
 		int wmId = LOWORD(wParam);
-		switch (wmId)
-		{
-		case IDC_BUTTON1: {
-			i = 0;
-			break;
-
-		}
-		case IDC_BUTTON2: {
-			i = 1;
-			break;
-
-		}
-		case IDC_BUTTON3: {
-			i = 2;
-			break;
-
-		}
 		
-			break;
-		}
 		
 	
 		
